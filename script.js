d814a6557deecf96f34c461e36a9a8f8b5edd581
developerdhruba <developerdhruba17@gmail.com>
2025-07-04 11:43:17 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
+// script.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+// Your Firebase config
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyA2eo2NdzNuQTr7FqKtKOuxs_zMjPshzWY",
+  authDomain: "pass-vault-61782.firebaseapp.com",
+  projectId: "pass-vault-61782",
+  storageBucket: "pass-vault-61782.firebasestorage.app",
+  messagingSenderId: "895384475260",
+  appId: "1:895384475260:web:92de42eb1355bacf1adbf9"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
